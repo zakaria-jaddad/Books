146f4ea8db747b18faec40023dbd8feb1f3087e5
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2023-04-22 03:27:04 +0000

    fixing some bugs and handelling %d function has an equivalente amounte of parameters

--- printf/printf.c
+++ printf/printf.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdarg.h>
 
     // ! write(int __fd, const void * __buf, size_t __nbyte)
 
     // * functions 
-void print(char *buffer);
+void print(const char *buffer, ...);
 
 int main(void) {
     // TODO : Get the buffer from the user 
@@ -40,13 +41,39 @@ int main(void) {
     }
 
 
-    print(buffer);
-    print("Ana Zakria This Is Just Some Test Here\n");
+    // print(buffer, 44, 45);
+    // print("Ana Zakria This Is Just Some Test Here\n");
+    print("Salam ana Zakaria 3andi %d l3am lwalid 3ando %d l3am \n", 19, 56);
     return 0;
     free(buffer);
     // print()
 }
 
-void print(char *buffer) {
-    write(1, buffer, strlen(buffer));
+void print(const char *buffer, ...) {
+
+    va_list args;
+    va_start(args, buffer);
+
+    int little_counter = -1;
+
+    // TODO : iterrating over the string  
+    for (int i = 0; buffer[i] != '\0' ; i++) {
+
+        // * if ther is the % sympole
+        if (buffer[i] == '%') {
+            
+            // TODO : handeling %d
+            if(buffer[i + 1] == 'd'){
+                little_counter = i + 1;
+                char number[100];
+                int num = va_arg(args, int);
+                sprintf(number, "%d", num);
+                write(1, number, strlen(number));
+            }
+        }
+        else if (i != little_counter)
+            write(1, &buffer[i], 1);
+
+    }
+    // write(1, buffer, strlen(buffer));
 }
