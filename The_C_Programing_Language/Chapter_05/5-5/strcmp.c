951011bd74337a11057581298b4f747081e7727b
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-05-25 17:17:42 +0100

    The C Book: Finished strcat, strend from 5-5 section

---
+++ The_C_Programing_Language/Chapter_05/5-5/strcmp.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int my_strcmp(char *s, char *t);
+
+int main(void) {
+
+  char *foo = "Hello, World!\n";
+  char *bar = "Hello, C Language!\n";
+
+  return 0;
+}
+
+int my_strcmp(char *s, char *t) {
+
+  for (; *s == *t; s++, t++) {
+    if (*s == '\0') {
+      return 0;
+    }
+  }
+  return *s - *t;
+}
+
+/*
+  This is a bit understandable
+  strcmp: return <0 if s<t, 0 if s==t, >0 if s>t
+  int strcmp(char *s, char *t)
+  {
+    int i;
+    for (i = 0; s[i] == t[i]; i++)
+      if (s[i] == '\0')
+        return 0;
+    return s[i] - t[i];
+  }
+ * */
