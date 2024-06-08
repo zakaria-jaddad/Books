a86eb0b49757fb54bd337f68b7d92ce1b6a73d20
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-06-08 19:23:39 +0100

    Finished 5.6 Pointer Arrays; Pointers to Pointers

--- The_C_Programing_Language/Chapter_05/5-6/sort_without_alloc/main.c
+++ The_C_Programing_Language/Chapter_05/5-6/sort_without_alloc/main.c
@@ -1,12 +1,12 @@
-#include "alloc/alloc.h"
 #include "getline/getline.h"
 #include "qsort/qsort.h"
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 
-#define MAXLINES 5000 /* max lines to be worted  */
-#define MAXLEN 1000   /* max length of any input line */
+#define MAXLINES 5000   /* max lines to be worted  */
+#define MAXLEN 1000     /* max length of any input line */
+#define ALLOCSIZE 10000 /* max allocation size */
 
 char *lineptr[MAXLINES]; /* pointers array to text line */
 
@@ -36,17 +36,28 @@ int main(void) {
 /* readlines: read input lines */
 int readlines(char *lineptr[], int maxlines) {
 
+  // allocation part
+  char allocbuff[ALLOCSIZE];
+  char *allocp = allocbuff;
+
   int len, nlines;
   char *p, line[MAXLEN];
 
   nlines = 0;
   while ((len = my_getline(line, MAXLEN)) > 0) {
-    if (nlines >= maxlines || (p = alloc(len)) == NULL) {
+
+    if (allocbuff + ALLOCSIZE - allocp < len || nlines >= maxlines) {
       return -1;
     }
+
+    p = allocp;
     line[len - 1] = '\0';
     strcpy(p, line);
+
     lineptr[nlines++] = p;
+
+    p = NULL;
+    allocp += len;
   }
   return nlines;
 }
