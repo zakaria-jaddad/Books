2d997bddd208854a1ace8f4a03b7a8e35f501c19
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-06-30 22:03:04 +0100

    Chapter 5 : Finished 5.10 Command-line Arguments

---
+++ The_C_Programing_Language/Chapter_05/5-10/tail.c
@@ -0,0 +1,130 @@
+/*
+  In this Exercise we need to make a programme that prints the last n lines
+  given by the user Example: ./tail -2
+
+  Since The program ask for input lines i'm going to use getline function
+  -2 is number of lines
+    -> hello, sir
+    -> hello, world
+    -> hello, man
+
+  output:
+  -> hello, world
+  -> hello, man
+*/
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLINE 1000
+#define MAXCHAR 1000
+
+int readlines(char **lineptr, int max_nlines);
+int my_getline(char *s, int limit);
+void writenlines(char *lineptr[], int line_index, int nlines);
+
+char *alloc(int n);
+
+int main(int argc, char **argv) {
+
+  /* line_number is -n == 10 */
+  int line_number = 10;
+  int current_nlines = 0;
+
+  char *lines[MAXLINE];
+  char *tail = *lines;
+
+  /* get line number from the given flag */
+  if (argc == 2) {
+    if (*(*(++argv))++ == '-' && isdigit(**argv)) {
+      line_number = atoi(*argv);
+    } else {
+      printf("Error: Unknown flag\n");
+    }
+  }
+
+  if ((current_nlines = readlines(lines, MAXLINE)) >= 0) {
+
+    if (current_nlines < line_number) {
+      printf("Error: flag -%d too big\n", line_number);
+      return -1;
+    }
+    if (current_nlines < line_number) {
+      printf("Error: -n is 10 by default\n");
+      return -1;
+    }
+    writenlines(lines, current_nlines - line_number, line_number);
+  }
+
+  return 0;
+}
+
+int readlines(char **lineptr, int max_nlines) {
+
+  int nlines = 0, len;
+  char *p, line[MAXCHAR];
+  int hello;
+
+  while ((len = my_getline(line, MAXCHAR)) > 0 &&
+         (nlines < max_nlines && (p = alloc(len)) != NULL)) {
+
+    line[len - 1] = '\0';
+    strcpy(p, line);
+    lineptr[nlines++] = p;
+  }
+
+  return nlines;
+}
+
+/*
+  my_getline clone of getline function
+  return int number of characters
+ */
+int my_getline(char *s, int limit) {
+
+  int c, i;
+
+  for (i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+    if (i + 1 == limit - 1) {
+      printf("Warning: Too Much Characters\n");
+      return i;
+    }
+
+    *s++ = c;
+  }
+
+  if (c == '\n') {
+    *s++ = c;
+    i++;
+  }
+  return i;
+}
+
+/*
+  Print all Lines
+ */
+void writenlines(char *lineptr[], int line_index, int nlines) {
+
+  int i = 0;
+  while (i++ < nlines) {
+    printf("%s\n", *(lineptr + (line_index++)));
+  }
+}
+
+#define ALLOCSIZE 10000
+
+char *alloc(int n);
+
+static char allocbuff[ALLOCSIZE]; /* storage for allocation */
+static char *allocp = allocbuff;  /* next free position */
+
+char *alloc(int n) {
+
+  if (allocbuff + ALLOCSIZE - allocp >= n) {
+    allocp += n;       /* alloc n slots */
+    return allocp - n; /* return the starting position of new allocation */
+  }
+  return 0;
+}
