e0cd36c890e8d3d6a426a76821a1055bd837660d
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-09-21 19:38:19 +0100

    Chapter 5:

--- The_C_Programing_Language/Chapter_05/5-06/sort_without_alloc/getline/getline.c
+++ The_C_Programing_Language/Chapter_05/5-06/sort_without_alloc/getline/getline.c
@@ -1,18 +1,22 @@
 #include "getline.h"
 #include <stdio.h>
+
 /*
   my_getline clone of getline function
   return int number of characters
  */
-int my_getline(char *s, int limit) {
+int my_getline(char *s, int limit)
+{
 
   int c, i;
 
-  for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+  for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+  {
     *(s + i) = c;
   }
 
-  if (c == '\n') {
+  if (c == '\n')
+  {
     *(s + i) = c;
     i++;
   }
